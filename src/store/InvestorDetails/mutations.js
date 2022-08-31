98cd27b68430d6010bf21495552acf02877ff629
Sujoy Sen <sujoysen@technowebs.in>
2022-08-31 13:10:13 +0530

    ~ Enhancement: Investor basic setup

---
+++ src/store/InvestorDetails/mutations.js
@@ -0,0 +1,25 @@
+export function setItems(state, payload) {
+  state.data = payload;
+}
+
+export function showHideCreateModal(state, payload) {
+  state.modals.createItem = payload;
+}
+
+export function setEditItem(state, payload) {
+  state.editItem.id = payload.id;
+  state.editItem.name = payload.name;
+  state.editItem.code = payload.code;
+  state.editItem.address_1 = payload.address_1;
+  state.editItem.address_2 = payload.address_2;
+  state.editItem.postcode = payload.postcode;
+  state.editItem.city_id = payload.city_id;
+}
+
+export function setEditModal(state, payload) {
+  state.modals.editItem = payload;
+}
+
+export function setLastUpdated(state, payload) {
+  state.lastUpdated = payload;
+}
