bae6c61516aab18d1a843ebd79e1f7dc6a8ca7c2
Sujoy Sen <sujoysen@technowebs.in>
2022-09-06 15:53:00 +0530

    ~ Fix: Minor issues, date & name in dashboard

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,14 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page class="flex flex-center bg-grey-3">
+    <q-card flat class="increase flex flex-center">
+      <q-card-section class="text-center">
+        <p class="header">Wed 17,</p>
+        <p class="sub-header text-weight-medium">September 2022</p>
+      </q-card-section>
+      <q-card-section class="header text-weight-light">
+        <p q-pl-lg>Welcome Amar Furniture</p>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
@@ -15,3 +19,16 @@ export default defineComponent({
   name: "IndexPage",
 });
 </script>
+
+<style scoped>
+.header {
+  font-size: 1.9rem;
+}
+.sub-header {
+  font-size: 1.2rem;
+}
+
+.increase {
+  width: 80vh;
+}
+</style>
